eca58a308a673b066a290a460b4c8e5f9ecbfd1d
Saman Khan <76584385+samankhan3210@users.noreply.github.com>
2023-03-07 13:06:09 +0500

    Add files via upload

---
+++ Semester 7/IPT/k190354_ipt_L1/k190354_Q1/k190354_Q1/k190354_Q1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+
+double a, b, c, sum;
+Console.Write("Enter first number : ");
+a = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter second number : ");
+b = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter third number : ");
+c = Convert.ToDouble(Console.ReadLine());
+sum = a + b + c;
+Console.WriteLine(string.Format("Sum of {0}, {1}, and {2} is = {3}", a, b, c, sum));
---
+++ Semester 7/IPT/k190354_ipt_L1/k190354_Q1/k190354_Q1/k190354_Q2.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+
+int len;
+string a;
+Console.Write("Enter any string : ");
+a = Convert.ToString(Console.ReadLine());
+len = a.Length - 1;
+Console.Write("{0} : ", a);
